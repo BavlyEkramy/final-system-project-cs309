a01b3bdb51804e46d91e9613b5fa4fd0a71407eb
bavly <bavlyakramy@gmail.com>
2023-12-01 03:09:12 +0200

    problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
+import Product from "./components/Product";
+import { useState, useEffect } from "react";
+
 function App() {
+  // const [cs309, setCs309] = useState("");
+  // const [productsFilter, setProductsFilter] = useState("");
+  const products = [
+    { name: "laptop1", id: "15" },
+    { name: "laptop2" },
+    { name: "laptop3" },
+    { name: "laptop4" },
+    { name: "laptop5" },
+    { name: "laptop6" },
+
+  ];
+  useEffect(() => {
+    let heart_icon = document.querySelectorAll(".product-item .product-info i")
+    heart_icon.forEach((z) => {
+      z.onclick = function () {
+        console.log("message")
+        if (z.className === "fas fa-heart") {
+          z.className = "far fa-heart";
+        } else z.className = "fas fa-heart";
+      }
+    })
+  })
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      {products.map((p) => (
+        <Product />
+      ))}
+    </>
   );
+
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('AllProduct-item'));
 root.render(
   <React.StrictMode>
     <App />
