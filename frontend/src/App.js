eedbe18badc0a2d9c66652fdc7af70849d5d3731
Yohana248 <152048862+Yohana248@users.noreply.github.com>
2023-12-01 08:47:53 +0200

    Yohana create a header and its router but delete router from global node_modules and install it in frontend folder to fix router issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
-function App() {
+import React from "react";
+import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+// import Product from './components/Product'
+// import Home from './components/Home'
+
+const App = () => {
+
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+    {/* Util complete other components */}
+      <Router>
+        <Routes>
+          <Route path={ROUTES.LANDING} element={<Header />}>
+            <Route path={ROUTES.HOME} element={<Header />} />
+          </Route>
+
+          {/* <Route path={ROUTES.PRODUCTS} element={<Product />} /> */}
+        </Routes>
+      </Router>
+  
+    </>
+  )
 }
 
 export default App;
