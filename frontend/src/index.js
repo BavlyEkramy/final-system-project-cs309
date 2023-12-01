db2f85ae70474f3cb1a85e1687137a2366d967c3
bavly <bavlyakramy@gmail.com>
2023-12-01 02:25:32 +0200

    adding product component ,font awesome for icons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('AllProduct-item'));
 root.render(
   <React.StrictMode>
     <App />
