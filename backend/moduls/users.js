573fd1b508267717d9e1f812170830b01ec743b5
Youssef <youssef01120646586@gmail.com>
2023-12-05 22:20:51 +0200

    add new requstes with signin and signup pages

--- backend/moduls/users.js
+++ backend/moduls/users.js
@@ -5,15 +5,15 @@ const Schema =mongoose.Schema ;
 const userSchema = new Schema({
 
     
-    firstName :{ type: String, required:true},
-    lasttName :{ type: String, required:true},
-    Email :{ type: String, required:true},
-    Password:{ type: String, required:true},
-    Adress:{ type: String, required:true},
-    Phone:{ type: String, required:true},
-    Date:{ type: Number, required:true},
-    Sex:{ type: String, required:true},
-    National:{ type: String, required:true},
+    firstName :{ type: String},
+    lasttName :{ type: String},
+    email :{ type: String, required:true},
+    password:{ type: String, required:true},
+    adress:{ type: String},
+    phone:{ type: String},
+    Age:{ type: Number},
+    sex:{ type: String},
+    national:{ type: String},
    
 });
 
--- frontend/src/components/Signin/Signin.jsx
+++ frontend/src/components/Signin/Signin.jsx
@@ -1,9 +1,49 @@
 import './index.css';
 
-const Signin = () => {
-  return (
-    <div className="signin">Signin</div>
-  );
+import axios from "axios";
+import React ,{ useState } from "react";
+import {useNavigate , NavLink} from "react-router-dom";
+function Signin() {
+    const history = useNavigate();
+    
+    const [email , setEmail] = useState('');
+    const [password , setPassword] = useState('');
+
+    async function Check(e) {
+        
+        e.preventDefault();
+        
+        try {
+            await axios.post("http://localhost:8000/login" , {email , password})
+            .then( res =>{
+                console.log(res.data);
+                if(res.data === "Email Dose Not Exist" || res.data === "Email or Password is not correct"){
+                    const p = document.createElement('p');
+                    p.textContent = res.data;
+                    const Signin = document.getElementsByClassName('Signin')[0];
+                    Signin.appendChild(p); 
+                }else{
+                    history('/home');
+                }
+            }).catch(error=>{
+                console.log(error);
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div className="Signin">
+            <h1>Login</h1>
+            <form action="GET">
+                <input type="email" placeholder="Email" onChange={(e)=> setEmail(e.target.value)} />
+                <input type="password" placeholder="Password" onChange={(e)=> setPassword(e.target.value)}/>
+                <button type="submit" onClick={Check}>Login</button>
+                <NavLink to="/signup">Signup</NavLink>
+            </form>
+        </div>
+    );
 }
 
 export default Signin;
